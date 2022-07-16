1a61fbc972161e7492b72df1a4ef2a02f09ec0ae
DaanS8 <72094257+DaanS8@users.noreply.github.com>
2022-07-16 14:03:48 +0200

    downloading drive file

--- PDF/drive_helper.py
+++ PDF/drive_helper.py
@@ -1,7 +1,12 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
+FILE_ID = "1gpryvBvfH31uZ7RAHPCw5WQdNKNU8sbmZ6GE96wpm-g"
+
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
+
+file = drive.CreateFile({'id': FILE_ID})
+file.GetContentFile('cursussen.xlsx', mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
